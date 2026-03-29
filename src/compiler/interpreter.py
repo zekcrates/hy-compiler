a2cb6a1fce3b9b0453f7444f6222386c77cf19f0
Manik verma <verma001manik@gmail.com>
2026-03-29 11:25:03 +0530

    some:

--- src/compiler/interpreter.py
+++ src/compiler/interpreter.py
@@ -27,5 +27,32 @@ def interpret(node: ast.Expression) -> Value:
                 return interpret(node.then_branch) 
             else:
                 return interpret(node.else_branch)
+        
+        case ast.UnaryOp():
+            c: Any = interpret(node.expr) 
+            if node.op == '-':
+                return -c
+            elif node.op == 'not':
+                return not c 
+            else:
+                raise Exception("Unknown error at unaryop") 
+
+        case ast.Function():
+           arg_vals = []
+           if node.args is not None:
+
+            for expr in node.args:
+                val = interpret(expr) 
+                arg_vals.append(val) 
+
+            if node.name == "print":
+                print(*arg_vals)
+                raise Exception("n")
+            else:
+                raise Exception("Unknown error at Function" ) 
+
+
         case _:
             raise Exception(f"Unsupported node type: {type(node).__name__}")
+
+    return None
